583c8e7513456bac5ba12b97b40b5f26cedd41f8
KIvanAn <kozel.vania2013@yandex.ru>
2022-01-08 11:59:03 +0300

    Day 5. Password generator v 1.0.1

--- day-5-password-generator/main.py
+++ day-5-password-generator/main.py
@@ -8,28 +8,26 @@ list_letters = list(map(chr, range(97, 123)))
 list_symbols = list("!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~")
 list_numbers = list(range(1, 10))
 
-password = []
+password_list = []
 
 for i in range(1, letters_count + 1):
-    get_letter = random.choices(list_letters)[0]
+    get_letter = random.choice(list_letters)
     is_Upper = random.randint(0, 1)
     letter = get_letter.upper() if is_Upper else get_letter
-    password.append(letter)
+    password_list.append(letter)
 
 for i in range(1, symbols_count + 1):
-    symbol = random.choices(list_symbols)[0]
-    password.append(symbol)
+    password_list.append(random.choice(list_symbols))
 
 for i in range(1, numbers_count + 1):
-    number = random.choices(list_numbers)[0]
-    password.append(str(number))
+    password_list.append(str(random.choice(list_numbers)))
 
-print(password)
+print(password_list)
 
-random.shuffle(password)
+random.shuffle(password_list)
 
-print(password)
+print(password_list)
 
-str_password = "".join(password)
+password = "".join(password_list)
 
-print(f"Your password is: {str_password}")
+print(f"Your password is: {password}")
